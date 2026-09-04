a74576c442ffdace191eb5081cfd8c7a5f633ab8
DENISE PEREIRA DA SILVA <denise.psilva19@senacsp.edu.br>
2026-09-03 21:56:24 -0300

    variaveis2

--- javascript/script.js
+++ javascript/script.js
@@ -11,3 +11,11 @@ a = 3
 b = (100+50*a)
 
 document.getElementById("resultado2").innerHTML= b
+
+function pegarvalor()
+{
+    ///pegando o elemento input pelo id e armazenamento em uma variavel
+    let sasa = document.getElementById("meuinput")
+    let valordigitado = sasa.value; ///exttrai o valor digitado usando o .value
+    document.getElementById("ola").innerHTML = valordigitado;
+}
