9ced16d9ed0154f3a73667ac16b2afaae9cb3ff8
DENISE PEREIRA DA SILVA <denise.psilva19@senacsp.edu.br>
2026-09-03 21:24:22 -0300

    variaveis2

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,13 @@
 document.getElementById("demo").innerHTML = "god"
+
+let x, y, z;
+x= 5
+y= 15
+z= x*y;
+document.getElementById("resultado").innerHTML= z
+
+let a, b;
+a = 3
+b = (100+50*a)
+
+document.getElementById("resultado2").innerHTML= b
